3733b5a54e855b30def693affcb50cb46468f38f
japesh <japeshsingla1@gmail.com>
2022-05-04 22:33:20 +0530

    use title from item

--- src/accordian/Accordian.tsx
+++ src/accordian/Accordian.tsx
@@ -1,90 +1,26 @@
-import React, { ReactElement, useCallback, useState } from 'react';
-import clsx from 'clsx';
-import logo from '../images/arrow-down.svg';
+import React, { ReactElement, useState } from 'react';
+import AccordianItem from "./AccordianItem"
 
-type AccordianItemProps = {
-  index: number;
-  isActive: boolean;
-  setActiveIndex: (index: number) => void;
-  children: ReactElement | string;
-};
-
-function AccordianItem({
-  index,
-  isActive,
-  setActiveIndex,
-  children,
-}: AccordianItemProps) {
-  const headingId = `accordianItemHeading${index}`;
-  const bodyId = `accordianItemBody${index}`;
-
-  const onClick = useCallback(() => {
-    setActiveIndex(isActive ? -1 : index);
-  }, [isActive, index]);
-
-  return (
-    <div className="accordionItem collapsed bg-white border border-gray-200">
-      <h2 className="accordionHeader mb-0" id={headingId}>
-        <button
-          className="
-  accordionButton
-  relative
-  flex
-  items-center
-  w-full
-  py-4
-  px-5
-  text-base text-gray-800 text-left
-  bg-white
-  border-0
-  rounded-none
-  transition
-  focus:outline-none
-  flex
-"
-          type="button"
-          aria-expanded={isActive}
-          aria-controls={bodyId}
-          onClick={onClick}
-        >
-          <span className="flex-1">Accordion Item #{index + 1}</span>
-          <img className="h-6" src={logo} />
-        </button>
-      </h2>
-      <div
-        id={bodyId}
-        className={clsx(
-          'accordionCollapse transition-all ease-in-out delay-1000 overflow-hidden',
-          {
-            'h-0': !isActive,
-          }
-        )}
-        aria-labelledby={headingId}
-      >
-        <div className="accordionBody py-4 px-5">{children}</div>
-      </div>
-    </div>
-  );
-}
+type Item = Object & { title: string };
 
 export default function Accordian({
   items,
   renderBody,
 }: {
-  items: Object[];
+  items: Item[];
   renderBody: (
-    item: Object,
+    item: Item,
     index: number,
-    items: Object[]
+    items: Item[]
   ) => ReactElement | string;
 }) {
   const [activeIndex, setActiveIndex] = useState(1);
-  // console.log('activeIndex', activeIndex);
   return (
     <div className="accordion">
       {items.map((item, index) => {
         return (
           <AccordianItem
+            title={item.title}
             index={index}
             isActive={activeIndex === index}
             setActiveIndex={setActiveIndex}
---
+++ src/accordian/AccordianItem.tsx
@@ -0,0 +1,70 @@
+import React, { ReactElement, useCallback } from 'react';
+import clsx from 'clsx';
+import logo from '../images/arrow-down.svg';
+
+type AccordianItemProps = {
+  index: number;
+  isActive: boolean;
+  setActiveIndex: (index: number) => void;
+  children: ReactElement | string;
+  title: string;
+};
+
+export default function AccordianItem({
+  index,
+  isActive,
+  setActiveIndex,
+  children,
+  title,
+}: AccordianItemProps) {
+  const headingId = `accordianItemHeading${index}`;
+  const bodyId = `accordianItemBody${index}`;
+
+  const onClick = useCallback(() => {
+    setActiveIndex(isActive ? -1 : index);
+  }, [isActive, index]);
+
+  return (
+    <div className="accordionItem collapsed bg-white border border-gray-200">
+      <h2 className="accordionHeader mb-0" id={headingId}>
+        <button
+          className="
+    accordionButton
+    relative
+    flex
+    items-center
+    w-full
+    py-4
+    px-5
+    text-base text-gray-800 text-left
+    bg-white
+    border-0
+    rounded-none
+    transition
+    focus:outline-none
+    flex
+  "
+          type="button"
+          aria-expanded={isActive}
+          aria-controls={bodyId}
+          onClick={onClick}
+        >
+          <span className="flex-1">{title}</span>
+          <img className="h-6" src={logo} />
+        </button>
+      </h2>
+      <div
+        id={bodyId}
+        className={clsx(
+          'accordionCollapse transition-all ease-in-out delay-1000 overflow-hidden',
+          {
+            'h-0': !isActive,
+          }
+        )}
+        aria-labelledby={headingId}
+      >
+        <div className="accordionBody py-4 px-5">{children}</div>
+      </div>
+    </div>
+  );
+}
