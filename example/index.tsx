1092ac61ab07bbbc7ba048a0d0b2ab4edb4b2b4c
japesh <japeshsingla1@gmail.com>
2022-05-04 19:10:12 +0530

    fixing import css issue

--- example/index.tsx
+++ example/index.tsx
@@ -1,9 +1,9 @@
-import 'react-app-polyfill/ie11';
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+// import 'react-app-polyfill/ie11';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { Accordian } from '../dist';
 
-// import "../dist/main.css";
+import "../dist/main.css";
 
 const App = () => {
   return (
